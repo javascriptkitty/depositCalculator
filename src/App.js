6eeb530f00c00a0b06b566c8d583b1f825f93a4c
Cristina.Alekseeva <cristina.alekseeva@gmail.com>
2020-07-31 17:36:07 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Container,
+  Card,
+  CardContent,
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl,
+  TextField,
+  Paper,
+} from "@material-ui/core";
+import data from "./depcalc.json";
+import "./App.scss";
 
 function App() {
+  const [selected, setSelected] = React.useState("");
+  const deposits = data.deposits;
+  const handleSelectChange = (event) => {
+    setSelected(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h2>Депозитный калькулятор</h2>
+      <Paper>
+        <Card>
+          <CardContent className="userInput">
+            {renderSelect(deposits, handleSelectChange, selected)}
+            <TextField required label="сумма" />
+            <TextField required label="срок вклада" />
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className="userOutput">
+            процентная ставка:
+            <br />
+            доход:
+          </CardContent>
+        </Card>
+      </Paper>
+    </Container>
   );
 }
 
 export default App;
+
+function renderSelect(data, handleChange, selected) {
+  return (
+    <FormControl required>
+      <InputLabel>тип вклада</InputLabel>
+      <Select value={selected} onChange={handleChange}>
+        {data.map((el) => {
+          return (
+            <MenuItem value={el.code} key={el.code}>
+              {el.name}
+            </MenuItem>
+          );
+        })}
+      </Select>
+    </FormControl>
+  );
+}
