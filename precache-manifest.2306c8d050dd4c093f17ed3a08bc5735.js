2db0e5bb33a75f7bfd6ab74555e4996758e558e7
Cristina.Alekseeva <cristina.alekseeva@gmail.com>
2020-08-02 21:53:10 +0300

    Updates

---
+++ precache-manifest.2306c8d050dd4c093f17ed3a08bc5735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77ce361b0a42885e46a1463b373fc1c",
+    "url": "/depositCalculator/index.html"
+  },
+  {
+    "revision": "0d007ffc37a533678105",
+    "url": "/depositCalculator/static/css/2.0ec5d33f.chunk.css"
+  },
+  {
+    "revision": "16e7bf431477fc455cbd",
+    "url": "/depositCalculator/static/css/main.627b20cb.chunk.css"
+  },
+  {
+    "revision": "0d007ffc37a533678105",
+    "url": "/depositCalculator/static/js/2.c11287f4.chunk.js"
+  },
+  {
+    "revision": "5795cac972af069c6d9a600320c75127",
+    "url": "/depositCalculator/static/js/2.c11287f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e7bf431477fc455cbd",
+    "url": "/depositCalculator/static/js/main.d5b7ce35.chunk.js"
+  },
+  {
+    "revision": "3c7e0549eb2eba820805",
+    "url": "/depositCalculator/static/js/runtime-main.1d31bce7.js"
+  }
+]);
